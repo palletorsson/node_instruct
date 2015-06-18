fd7a7761f5566f82a8bfbd95d78afc1ea32042cc
plix <palle.torsson@gmail.com>
2015-06-18 18:13:42 +0200

    Update server.js

--- static/server.js
+++ static/server.js
@@ -1,1 +1,10 @@
 
+var express = require('express'); 
+var app = express(); 
+app.use(express.static(__dirname + '/public'));  
+app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+var port= process.env.PORT || 8080;
+app.listen(port); 
+console.log("go to http://localhost:8080"); 
